b786f3572f60b1551eff6e9211bec7d25463eef8
machina-source <exxxmachina@protonmail.com>
2024-12-12 17:54:21 -0600

    Fix websocket connection error
    Fix genexplainer demo connection error
    Fix API key error typo

--- src/App.tsx
+++ src/App.tsx
@@ -24,10 +24,10 @@ import Footer from "./components/footer/Footer";
 
 const API_KEY = process.env.REACT_APP_GEMINI_API_KEY as string;
 if (typeof API_KEY !== "string") {
-  throw new Error("set REACT_APP_GEMINI_APIK_KEY in .env");
+  throw new Error("set REACT_APP_GEMINI_API_KEY in .env");
 }
 
-const host = "autopush-generativelanguage.googleapis.com";
+const host = "generativelanguage.googleapis.com";
 const uri = `wss://${host}/ws/google.ai.generativelanguage.v1alpha.GenerativeService.BidiGenerateContent`;
 
 function App() {
